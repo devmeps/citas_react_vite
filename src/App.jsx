fec0c4791927d4c54c3217241a35be7a3616385c
Maximo Eduardo <theprojectall@outlook.com>
2023-10-01 13:41:51 -0500

    Primer Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState, useEffect } from "react"
-import Header from "./components/header"
+import { useState, useEffect } from 'react'
 import Formulario from "./components/Formulario"
+import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
-
 
 function App() {
 
-  const [pacientes, setPacientes] = useState ([]);
+  const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
 
   useEffect(() => {
@@ -15,36 +14,34 @@ function App() {
       setPacientes(pacientesLS)
     }
     obtenerLS();
-  })
+  }, []);
 
-  useEffect(()=> {
+  useEffect(() => {
     localStorage.setItem('pacientes', JSON.stringify( pacientes ));
   }, [pacientes])
 
   const eliminarPaciente = id => {
     const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id);
     setPacientes(pacientesActualizados)
-
   }
-
 
   return (
     <div className="container mx-auto mt-20">
-    <Header />
+      <Header />
+
       <div className="mt-12 md:flex">
-        <Formulario
-        pacientes={pacientes}
-        setPacientes={setPacientes}
-        paciente={paciente}
-        setPaciente={setPaciente} 
-    
-        />
-        <ListadoPacientes
-        pacientes = {pacientes}
-        setPaciente={setPaciente}
-        eliminarPaciente = {eliminarPaciente}
-        />
-     </div>
+          <Formulario 
+            pacientes={pacientes}
+            setPacientes={setPacientes}
+            paciente={paciente}
+            setPaciente={setPaciente}
+          />
+          <ListadoPacientes 
+            pacientes={pacientes}
+            setPaciente={setPaciente}
+            eliminarPaciente={eliminarPaciente}
+          />
+      </div>
 
     </div>
   )
--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
 const Error = ({children}) => {
     return(
         <div className="bg-red-800 text-white text-center p-3 uppercase mb-3 rounded-lg font-bold">
-                <p>{children}</p>
+                <p>{childrgien}</p>
                 </div>
     )
 }
